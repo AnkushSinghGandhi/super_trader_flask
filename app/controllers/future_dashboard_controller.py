7d1324749f4ee5dad97b4f57c7ed5549e5180d99
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-04-30 15:50:26 +0530

    added future dashboard and heatmap controller

---
+++ app/controllers/future_dashboard_controller.py
@@ -0,0 +1,149 @@
+## get_last_quote_Array.json
+## futures_list = json_data[0]['Result']
+from flask import jsonify, request
+from app.models.last_quote_array import LastQuoteArrayModel
+
+
+class FutureDashboardController:
+    @staticmethod
+    def future_dashboard():
+        status = request.args.get('status')
+
+        # Fetch data from MongoDB collection
+        futures_list = LastQuoteArrayModel.get_last_quote_array()
+
+        if status.lower() == 'price_change':
+            sorted_futures = sorted(futures_list, key=lambda x: x.get('PriceChangePercentage', 0), reverse=True)
+
+            top_10_highest = sorted_futures[:10]
+            top_10_lowest = sorted_futures[-10:]
+
+            response = {
+                'Message': 'Success',
+                'status': 200,
+                'data': {
+                    'top_10_highest': top_10_highest,
+                    'top_10_lowest': top_10_lowest
+                }
+            }
+
+            return jsonify(response)
+
+        if status == 'oi_change':
+            sorted_futures = sorted(futures_list, key=lambda x: x.get('OpenInterestChange', 0), reverse=True)
+            top_10_highest = sorted_futures[:10]
+
+            response = {
+                'Message': 'Success',
+                'status': 200,
+                'data': top_10_highest
+            }
+
+            return jsonify(response)
+
+        if status.lower() == 'long_buildup':
+            sorted_futures = sorted(futures_list,
+                                    key=lambda x: (x.get('OpenInterestChange', 0), x.get('PriceChangePercentage', 0)),
+                                    reverse=True)
+
+            top_10 = sorted_futures[:10]
+
+            response = {
+                'message': 'Success',
+                'status': 200,
+                'data': {
+                    'top_10': top_10
+                }
+            }
+
+            return jsonify(response)
+
+        if status.lower() == 'short_buildup':
+            sorted_futures = sorted(futures_list,
+                                    key=lambda x: (x.get('OpenInterestChange', 0), x.get('PriceChangePercentage', 0)))
+
+            top_10 = sorted_futures[:10]
+
+            response = {
+                'message': 'Success',
+                'status': 200,
+                'data': {
+                    'top_10': top_10
+                }
+            }
+
+            return jsonify(response)
+
+        if status.lower() == 'short_covering':
+            sorted_futures = sorted(futures_list,
+                                    key=lambda x: (x.get('OpenInterestChange', 0), x.get('PriceChangePercentage', 0)),
+                                    reverse=True)
+
+            top_10 = sorted_futures[:10]
+
+            response = {
+                'message': 'Success',
+                'status': 200,
+                'data': {
+                    'top_10': top_10
+                }
+            }
+
+            return jsonify(response)
+
+        if status.lower() == 'long_unwinding':
+            sorted_futures = sorted(futures_list,
+                                    key=lambda x: (x.get('OpenInterestChange', 0), x.get('PriceChangePercentage', 0)))
+
+            top_10 = sorted_futures[:10]
+
+            response = {
+                'message': 'Success',
+                'status': 200,
+                'data': {
+                    'top_10': top_10
+                }
+            }
+
+            return jsonify(response)
+
+        if status.lower() == 'highest_turnover':
+            sorted_futures = sorted(futures_list, key=lambda x: x.get('Turnover', 0), reverse=True)
+
+            top_10 = sorted_futures[:10]
+
+            response = {
+                'message': 'Success',
+                'status': 200,
+                'data': {
+                    'top_10': top_10
+                }
+            }
+
+            return jsonify(response)
+
+        if status.lower() == 'iv_change':
+            sorted_futures_highest = sorted(futures_list, key=lambda x: x.get('IVChangePercentage', 0), reverse=True)
+            sorted_futures_lowest = sorted(futures_list, key=lambda x: x.get('IVChangePercentage', 0))
+
+            top_10_highest = sorted_futures_highest[:10]
+            top_10_lowest = sorted_futures_lowest[:10]
+
+            response = {
+                'message': 'Success',
+                'status': 200,
+                'data': {
+                    'top_10_highest': top_10_highest,
+                    'top_10_lowest': top_10_lowest
+                }
+            }
+
+            return jsonify(response)
+
+        response = {
+            'message': 'Invalid status',
+            'status': 400,
+            'data': None
+        }
+
+        return jsonify(response)
---
+++ app/controllers/heatmap_controller.py
@@ -0,0 +1,107 @@
+from flask import jsonify, request
+from app.models.last_quote_array import LastQuoteArrayModel
+
+
+class HeatMapController:
+    @staticmethod
+    def get_heat_map():
+        last_quote_array_data = LastQuoteArrayModel.get_last_quote_array()
+
+        # Constants
+        nifty50_constituents = [
+            'AdaniPorts-I',
+            'Adani-Enterprises-I',
+            'Apollo-Hospitals-I',
+            'Axis-Bank-I',
+            'Hero-MotoCorp',
+            'ICICI-Bank',
+            'ITC',
+            'Infosys',
+            'Indusind-Bank',
+            'JSW-Steel',
+            'Kotak-Mahindra-Bank',
+            'Larsen-&-Toubro',
+            'LTIMindtree',
+            'Mahindra-and-Mahindra',
+            'Maruti-Suzuki',
+            'Nestle',
+            'Oil-and-Natural-Gas-Corporation',
+            'NTPC',
+            'Power-Grid-Corporation-of-India',
+            'Reliance-Industries',
+            'State-Bank-of-India',
+            'SBI-Life-Insurance-Company',
+            'Sun-Pharmaceutical-Industries',
+            'TATASTEEL',
+            'Tata-Consumer-Products',
+            'Tata-Motors',
+            'TCS',
+            'TECHM',
+            'Titan-Company',
+            'UPL',
+            'ULTRACEMCO',
+            'Wipro',
+        ]
+        banknifty_constituents = [
+            'HDFC-Bank',
+            'ICICI-Bank',
+            'State-Bank-of-India-(SBI)',
+            'Kotak-Bank',
+            'Axis-Bank',
+            'IndusInd-Bank',
+            'Punjab-National-Bank',
+            'Bank-of-Baroda',
+            'Federal-Bank',
+            'IDFC-First-Bank',
+            'RBL-Bank',
+            'AUBank'
+        ]
+
+        instrument_server_time = {}
+
+        for quote in last_quote_array_data:
+            InstrumentIdentifier = quote["InstrumentIdentifier"]
+            server_time = quote["ServerTime"]
+
+            if InstrumentIdentifier in instrument_server_time:
+                if server_time > instrument_server_time[InstrumentIdentifier]:
+                    instrument_server_time[InstrumentIdentifier] = server_time
+            else:
+                instrument_server_time[InstrumentIdentifier] = server_time
+
+        results_nifty50 = []
+        results_banknifty = []
+        results_fno = []
+
+        for quote2 in last_quote_array_data:
+            InstrumentIdentifier = quote2["InstrumentIdentifier"]
+            ltp = quote2["LastTradePrice"]
+            price_percentage_change = quote2["PriceChangePercentage"]
+            server_time = quote2["ServerTime"]
+
+            if server_time == instrument_server_time[InstrumentIdentifier]:
+                if any(value.lower() in InstrumentIdentifier.lower() for value in nifty50_constituents):
+                    results_nifty50.append({
+                        'InstrumentIdentifier': InstrumentIdentifier,
+                        'LTP': ltp,
+                        'PricePercentageChange': price_percentage_change
+                    })
+
+                if any(value.lower() in InstrumentIdentifier.lower() for value in banknifty_constituents):
+                    results_banknifty.append({
+                        'InstrumentIdentifier': InstrumentIdentifier,
+                        'LTP': ltp,
+                        'PricePercentageChange': price_percentage_change
+                    })
+
+                results_fno.append({
+                    'InstrumentIdentifier': InstrumentIdentifier,
+                    'LTP': ltp,
+                    'PricePercentageChange': price_percentage_change
+                })
+
+        return jsonify({
+            'Nifty50': results_nifty50,
+            'Banknifty': results_banknifty,
+            'FNO': results_fno
+        })
