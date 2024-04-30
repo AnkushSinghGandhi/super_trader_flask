64c5bb35766fbd5c89f87f999b168e0b0cee13eb
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-04-30 22:06:45 +0530

    Update config.py

--- app/trade_service/config.py
+++ app/trade_service/config.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 import certifi
 
-client = MongoClient('mongodb+srv://manthangehlot66:rahulsharma@optionaro.f2tgbib.mongodb.net/?retryWrites=true&w=majority&appName=OptionARO', tlsCAFile=certifi.where())
-db = client['OptionARO']
+client = MongoClient('', tlsCAFile=certifi.where())
+db = client['SuperTrader']
