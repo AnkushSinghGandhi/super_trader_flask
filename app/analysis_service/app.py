0658441a9472f8fd845d1aa0ea21343992e5c486
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-05-06 07:28:04 +0530

    added contrller and routes for stock data

--- app/analysis_service/app.py
+++ app/analysis_service/app.py
@@ -9,13 +9,77 @@ from controllers.option_chain_controller import OptionChainController
 from controllers.option_dashboard_controller import OptionDashboardController
 from controllers.implied_volatility_controller import ImpliedVolatilityController
 from controllers.pcr_controller import PcrController
-
+from controllers.last_quote_controller import LastQuoteController
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 # Swagger configuration
 swagger = Swagger(app)
+
+# Routes for fetching stock data
+@app.route('/get_ltp', methods=['GET'])
+def get_ltp():
+    """
+    Get the last trade price of a stock by instrument identifier.
+    ---
+    tags:
+      - Stock
+    parameters:
+      - name: instrument_identifier
+        in: query
+        type: string
+        required: true
+        description: Instrument identifier of the stock
+    responses:
+      200:
+        description: Successful operation
+      404:
+        description: Instrument not found
+    """
+    instrument_identifier = request.args.get('instrument_identifier')
+    ltp = LastQuoteController.get_ltp(instrument_identifier)
+    if ltp is not None:
+        # Emit last trade price data via WebSocket
+        socketio.emit('ltp_update', {'InstrumentIdentifier': instrument_identifier, 'LastTradePrice': ltp})
+        return jsonify({'InstrumentIdentifier': instrument_identifier, 'LastTradePrice': ltp})
+    else:
+        return jsonify({'error': 'Instrument not found'}), 404
+
+@app.route('/get_all_instrument_identifiers', methods=['GET'])
+def get_all_instrument_identifiers():
+    """
+    Get all instrument identifiers of available stocks.
+    ---
+    tags:
+      - Stock
+    responses:
+      200:
+        description: Successful operation
+    """
+    return LastQuoteController.get_all_instrument_identifiers()
+
+@app.route('/get_quote_details', methods=['GET'])
+def get_quote_details():
+    """
+    Get the details of a stock by instrument identifier.
+    ---
+    tags:
+      - Stock
+    parameters:
+      - name: instrument_identifier
+        in: query
+        type: string
+        required: true
+        description: Instrument identifier of the stock
+    responses:
+      200:
+        description: Successful operation
+      404:
+        description: Instrument details not found
+    """
+    instrument_identifier = request.args.get('instrument_identifier')
+    return LastQuoteController.get_quote_details(instrument_identifier)
 
 
 # Routes for Option Dashboard
@@ -43,10 +107,13 @@ def get_implied_volatility_chart():
 # Routes for Future Dashboard
 @app.route('/future_dashboard', methods=['GET'])
 def get_future_dashboard():
-    return FutureDashboardController.get_future_dashboard()
+    return FutureDashboardController.get_future_dashboard_data()
 
 
 # Routes for Heat Map
 @app.route('/heat_map', methods=['GET'])
 def get_heat_map():
     return HeatMapController.get_heat_map()
+
+if __name__ == '__main__':
+    app.run(debug=True)
