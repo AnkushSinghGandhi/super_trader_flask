4f1dbb60a695602637fdfeac742321c569782c74
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2024-05-07 15:01:43 +0530

    update: added jwt configs

--- app/user_service/config.py
+++ app/user_service/config.py
@@ -3,3 +3,7 @@ import certifi
 
 client = MongoClient('', tlsCAFile=certifi.where())
 db = client['SuperTrader']
+
+class Config:
+    SECRET_KEY = 'your_secret_key_here'
+    JWT_SECRET_KEY = 'your_jwt_secret_key_here'
